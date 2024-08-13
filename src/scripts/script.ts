1cefce69b31369d442d0d6b3466820f68980ca53
Juston Hart <contact@justonh.art>
2024-08-13 14:39:00 -0500

    Set up basic UI and styling for gameplay

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -2,7 +2,8 @@ import '../style.css';
 import Player from './Player';
 
 let isPlayerGame: boolean;
-const gameboardElement = document.querySelector('.gameboard') as HTMLDivElement;
+const targetBoard = document.getElementById('targetBoard') as HTMLDivElement;
+const playerBoard = document.getElementById('playerBoard') as HTMLDivElement;
 
 let players: Player[] = [];
 players.push(new Player());
@@ -10,13 +11,13 @@ players.push(new Player());
 
 let currentPlayerIndex = 0;
 
-drawGameboard(currentPlayerIndex);
+renderGameState(currentPlayerIndex);
 
 /**
  * Render a player's moveMatrix inside the Gameboard element
  */
-function drawGameboard(playerIndex: number): void {
-	gameboardElement.innerHTML = '';
+function renderGameState(playerIndex: number): void {
+	targetBoard.innerHTML = '';
 	for (let i = 0; i < 9; i++) {
 		for (let j = 0; j < 9; j++) {
 			let space = document.createElement('div');
@@ -33,7 +34,32 @@ function drawGameboard(playerIndex: number): void {
 					space.classList.add('miss');
 				}
 			}
-			gameboardElement.appendChild(space);
+			targetBoard.appendChild(space);
+		}
+	}
+
+	playerBoard.innerHTML = '';
+	let playerBoardAttackData = players[playerIndex].Gameboard.getAttacks();
+	for (let i = 0; i < 9; i++) {
+		for (let j = 0; j < 9; j++) {
+			let space = document.createElement('div');
+			space.classList.add('space');
+			space.dataset.row = i.toString();
+			space.dataset.col = j.toString();
+
+			let attack = playerBoardAttackData.find(
+				(attack) => attack.x === i && attack.y === j,
+			);
+			if (attack != undefined) {
+				console.log(attack);
+				if (attack.hit) {
+					space.innerText = '💥';
+					space.classList.add('hit');
+				} else {
+					space.classList.add('miss');
+				}
+			}
+			playerBoard.appendChild(space);
 		}
 	}
 }
@@ -41,12 +67,14 @@ function drawGameboard(playerIndex: number): void {
 addEventListener('click', (event: Event) => {
 	if (
 		event.target instanceof HTMLDivElement &&
+		event.target.parentElement.id === 'targetBoard' &&
 		event.target.classList.contains('space')
 	) {
 		let targetPlayer = players[(currentPlayerIndex + 1) % 2];
 		let x = parseInt(event.target.dataset.row);
 		let y = parseInt(event.target.dataset.col);
 		players[currentPlayerIndex].makeMove(targetPlayer, x, y);
-		drawGameboard(currentPlayerIndex);
+		targetPlayer.Gameboard.receiveAttack(x, y);
+		renderGameState(currentPlayerIndex);
 	}
 });
